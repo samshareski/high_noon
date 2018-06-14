123513f5400042642e543eab8d3856c604f1c47f
Sam Shareski <sam_shareski@otpp.com>
2018-06-14 12:01:48 -0400

    Remove name from game server process

--- lib/high_noon/game_server.ex
+++ lib/high_noon/game_server.ex
@@ -2,7 +2,7 @@ defmodule HighNoon.GameServer do
   use GenServer
 
   def start_link do
-    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+    GenServer.start_link(__MODULE__, :ok)
   end
 
   def init(args) do
