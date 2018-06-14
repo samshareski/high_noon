69b1f2419ba0818ef5afc229adf146112bd5f6dd
Sam Shareski <sam_shareski@otpp.com>
2018-06-14 17:23:37 -0400

    Add game server

--- lib/high_noon/game_server.ex
+++ lib/high_noon/game_server.ex
@@ -1,11 +1,75 @@
 defmodule HighNoon.GameServer do
   use GenServer
 
+  alias HighNoon.Game
+
+  @timer_median :timer.seconds(5)
+  @timer_range :timer.seconds(6)
+
+  # Client
+
   def start_link do
     GenServer.start_link(__MODULE__, :ok)
   end
 
-  def init(args) do
-    {:ok, args}
+  def start_game(pid) do
+    GenServer.call(pid, :start_game)
+  end
+
+  def start_fixed(pid, time) do
+    GenServer.call(pid, {:start_fixed, time})
+  end
+
+  def player_1_fire(pid) do
+    GenServer.call(pid, :player_1_fire)
+  end
+
+  def player_2_fire(pid) do
+    GenServer.call(pid, :player_2_fire)
+  end
+
+  # Server
+
+  def init(_args) do
+    {:ok, Game.new()}
+  end
+
+  def handle_call(:start_game, from, game) do
+    new_game = Game.start(game)
+    Process.send_after(self(), {:high_noon, from}, random_high_noon())
+    {:reply, new_game, new_game}
+  end
+
+  def handle_call({:start_fixed, time}, from, game) do
+    new_game = Game.start(game)
+    Process.send_after(self(), {:high_noon, from}, time)
+    {:reply, new_game, new_game}
+  end
+
+  def handle_call(:player_1_fire, _from, game) do
+    new_game = Game.player_1_fire(game)
+    {:reply, new_game, new_game}
+  end
+
+  def handle_call(:player_2_fire, _from, game) do
+    new_game = Game.player_2_fire(game)
+    {:reply, new_game, new_game}
+  end
+
+  def handle_info({:high_noon, from}, game) do
+    new_game = Game.strike_noon(game)
+    GenServer.reply(from, {:high_noon, new_game})
+    {:noreply, new_game}
+  end
+
+  def handle_info(_msg, game) do
+    {:noreply, game}
+  end
+
+  # Helpers
+
+  defp random_high_noon() do
+    offset = :rand.uniform(@timer_range) - div(@timer_range, 2)
+    @timer_median + offset
   end
 end
--- test/game_server_test.exs
+++ test/game_server_test.exs
@@ -1,9 +1,55 @@
 defmodule GameServerTest do
   use ExUnit.Case
 
-  alias HighNoon.GameServer
+  alias HighNoon.{GameServer, Game}
 
   test "spawning a game server process" do
     assert {:ok, _pid} = GameServer.start_link()
+  end
+
+  test "start a game in the game server" do
+    assert {:ok, pid} = GameServer.start_link()
+
+    game = GameServer.start_game(pid)
+
+    assert %Game{started: true} = game
+  end
+
+  test "recieve high noon message after starting game" do
+    assert {:ok, pid} = GameServer.start_link()
+
+    GameServer.start_fixed(pid, 100)
+
+    assert_receive {_, {:high_noon, %{started: true, high_noon: true}}}, :timer.seconds(2)
+  end
+
+  test "player fires before noon and loses" do
+    assert {:ok, pid} = GameServer.start_link()
+
+    GameServer.start_fixed(pid, 100)
+    assert %{player_1_status: :backfired} = GameServer.player_1_fire(pid)
+
+    assert_receive {_, {:high_noon, %{winner: :player_2}}}, :timer.seconds(2)
+  end
+
+  test "player fires after noon and wins" do
+    assert {:ok, pid} = GameServer.start_link()
+
+    GameServer.start_fixed(pid, 100)
+
+    assert_receive {_, {:high_noon, %{winner: nil}}}, :timer.seconds(2)
+
+    assert %{winner: :player_1} = GameServer.player_1_fire(pid)
+  end
+
+  test "both players fire before noon and draw" do
+    assert {:ok, pid} = GameServer.start_link()
+
+    GameServer.start_fixed(pid, 100)
+
+    assert %{player_1_status: :backfired} = GameServer.player_1_fire(pid)
+    assert %{player_2_status: :backfired} = GameServer.player_2_fire(pid)
+
+    assert_receive {_, {:high_noon, %{winner: :draw}}}, :timer.seconds(2)
   end
 end
