1b11190258a1d9fa4ee8424f7c5eadd253cf418d
Sam Shareski <sam_shareski@otpp.com>
2018-06-14 11:51:41 -0400

    Completed game struct

--- lib/high_noon/game.ex
+++ lib/high_noon/game.ex
@@ -15,17 +15,53 @@ defmodule HighNoon.Game do
     %{game | started: true}
   end
 
-  def strike_noon(game) do
+  def strike_noon(%{started: true} = game) do
     %{game | high_noon: true}
     |> check_winner
   end
 
+  def strike_noon(game) do
+    game
+  end
+
   def player_1_fire(%{started: true, winner: nil, player_1_status: :fine} = game) do
     case game.high_noon do
-      true -> %{game | player_2_status: :shot, winner: :player_1}
-      false -> %{game | player_1_status: :backfied}
+      true -> %{game | player_2_status: :shot}
+      false -> %{game | player_1_status: :backfired}
     end
     |> check_winner
+  end
+
+  def player_1_fire(game) do
+    game
+  end
+
+  def player_2_fire(%{started: true, winner: nil, player_2_status: :fine} = game) do
+    case game.high_noon do
+      true -> %{game | player_1_status: :shot}
+      false -> %{game | player_2_status: :backfired}
+    end
+    |> check_winner
+  end
+
+  def player_2_fire(game) do
+    game
+  end
+
+  defp check_winner(%{high_noon: true} = game) do
+    cond do
+      game.player_1_status == :fine and game.player_2_status != :fine ->
+        %{game | winner: :player_1}
+
+      game.player_2_status == :fine and game.player_2_status != :fine ->
+        %{game | winner: :player_2}
+
+      game.player_2_status != :fine and game.player_2_status != :fine ->
+        %{game | winner: :draw}
+
+      true ->
+        game
+    end
   end
 
   defp check_winner(game) do
--- test/game_test.exs
+++ test/game_test.exs
@@ -6,7 +6,6 @@ defmodule GameTest do
   test "creates a default struct" do
     assert %Game{
              started: false,
-             ended: false,
              high_noon: false,
              player_1_status: :fine,
              player_2_status: :fine,
@@ -18,5 +17,85 @@ defmodule GameTest do
     game = Game.new() |> Game.start()
 
     assert %{started: true} = game
+  end
+
+  test "functions won't change game before game starts" do
+    game = Game.new()
+    new_game = Game.player_1_fire(game)
+
+    assert game == new_game
+
+    game = Game.new()
+    new_game = Game.player_2_fire(game)
+
+    assert game == new_game
+
+    game = Game.new()
+    new_game = Game.strike_noon(game)
+
+    assert game == new_game
+  end
+
+  test "high noon before any shots doesn't end game" do
+    game = Game.new() |> Game.start() |> Game.strike_noon()
+
+    assert %{winner: nil, high_noon: true} = game
+  end
+
+  test "player fire before high noon backfires" do
+    game = Game.new() |> Game.start() |> Game.player_1_fire()
+
+    assert %{player_1_status: :backfired} = game
+
+    game = Game.new() |> Game.start() |> Game.player_2_fire()
+
+    assert %{player_2_status: :backfired} = game
+  end
+
+  test "player fire after high noon wins" do
+    game =
+      Game.new()
+      |> Game.start()
+      |> Game.strike_noon()
+      |> Game.player_1_fire()
+
+    assert %{player_2_status: :shot, winner: :player_1}
+
+    game =
+      Game.new()
+      |> Game.start()
+      |> Game.strike_noon()
+      |> Game.player_2_fire()
+
+    assert %{player_1_status: :shot, winner: :player_2}
+  end
+
+  test "high noon after one backfire ends game" do
+    game =
+      Game.new()
+      |> Game.start()
+      |> Game.player_1_fire()
+      |> Game.strike_noon()
+
+    assert %{player_1_status: :backfired, winner: :player_2}
+
+    game =
+      Game.new()
+      |> Game.start()
+      |> Game.player_2_fire()
+      |> Game.strike_noon()
+
+    assert %{player_2_status: :backfired, winner: :player_1}
+  end
+
+  test "high noon after two backfires ends game in draw" do
+    game =
+      Game.new()
+      |> Game.start()
+      |> Game.player_1_fire()
+      |> Game.player_2_fire()
+      |> Game.strike_noon()
+
+    assert %{player_1_status: :backfired, player_2_status: :backfired, winner: :draw}
   end
 end
